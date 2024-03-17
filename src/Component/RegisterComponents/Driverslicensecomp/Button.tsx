b1d4a79567f2c5fc41028bca05954f3e19d15abe
Nizam <ramazanov570633@gmail.com>
2024-03-17 20:26:27 +0400

    End - 3

--- src/Component/RegisterComponents/Driverslicensecomp/Button.tsx
+++ src/Component/RegisterComponents/Driverslicensecomp/Button.tsx
@@ -1,15 +1,15 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { SCREEN_WIDTH } from '../../../Utils/common'
+// import { View, Text, TouchableOpacity } from 'react-native'
+// import React, { useState } from 'react'
+// import { SCREEN_WIDTH } from '../../../Utils/common'
 
-const Button = ({navigation}:any) => {
-    return (
-        <View style={{ width: SCREEN_WIDTH - 40, alignSelf: 'center' }}>
-            <TouchableOpacity style={{backgroundColor:'rgba(0, 80, 155, 1)', borderRadius:10}} onPress={()=> navigation.navigate('WelcomeScreen')}>
-                <Text style={{color:'rgba(255, 255, 255, 1)', textAlign:'center', fontSize:17, padding:'2%'}}>Müraciət edin</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+// const Button = ({navigation}:any) => {
+//     return (
+//         <View style={{ width: SCREEN_WIDTH - 40, alignSelf: 'center' }}>
+//             <TouchableOpacity style={{backgroundColor:'rgba(0, 80, 155, 1)', borderRadius:10}} onPress={()=> navigation.navigate('WelcomeScreen')}>
+//                 <Text style={{color:'rgba(255, 255, 255, 1)', textAlign:'center', fontSize:17, padding:'3%'}}>Müraciət edin</Text>
+//             </TouchableOpacity>
+//         </View>
+//     )
+// }
 
-export default Button
+// export default Button
