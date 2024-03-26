403340f025313a5e468cd901d6911ae4777b5ed7
Nizam <ramazanov570633@gmail.com>
2024-03-26 18:24:34 +0400

    Home update new 3

--- src/Component/HomeComponent/Online/EnoughView/EnoughView.tsx
+++ src/Component/HomeComponent/Online/EnoughView/EnoughView.tsx
@@ -1,8 +1,10 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import KM from '../../../../assets/imge/Home-imge/Icon set.svg'
 import Oclock from '../../../../assets/imge/Home-imge/icon.svg'
 import { SCREEN_WIDTH } from '../../../../Utils/common'
+import Callsvg from '../../../../assets/imge/Home-imge/Frame.svg'
+import Moresvg from '../../../../assets/imge/Home-imge/mor.svg'
 const EnoughView = () => {
     return (
         <View style={{ width: SCREEN_WIDTH - 40, alignSelf: 'center' }}>
@@ -18,7 +20,7 @@ const EnoughView = () => {
                 <View style={{ display: 'flex', flexDirection: 'row' }}>
                     <View style={{ flexDirection: 'column', alignItems: 'center', display: "flex" }}>
                         <View style={{ width: 20, height: 20, backgroundColor: 'rgba(31, 94, 170, 1)', borderRadius: 50 }}></View>
-                        <View style={{ width: 5, height: 180, backgroundColor: 'rgba(118, 119, 122, 1)', borderRadius: 0 }}></View>
+                        <View style={{ width: 5, height: 165, backgroundColor: 'rgba(118, 119, 122, 1)', borderRadius: 0 }}></View>
                         <View style={{ width: 20, height: 20, backgroundColor: 'rgba(118, 119, 122, 1)', borderRadius: 50 }}></View>
                     </View>
 
@@ -27,62 +29,107 @@ const EnoughView = () => {
                     {/*  Flatlist ilə yazılacaq */}
                     <View style={{ marginLeft: '3%' }}>
                         <View>
-                            <Text style={{ color: "rgba(144, 144, 148, 1)", }}>Qəbul nöqtəsi</Text>
-                            <Text style={{ color: "rgba(16, 17, 20, 1)", marginTop: '2%', fontSize: 15 }}>Şamaxı, yuxarı şilyan</Text>
+                            <Text style={{ color: "rgba(144, 144, 148, 1)", fontSize: 10 }}>Qəbul nöqtəsi</Text>
+                            <Text style={{ color: "rgba(16, 17, 20, 1)", fontSize: 11 }}>Şamaxı, yuxarı şilyan</Text>
                             <View style={{ display: 'flex', flexDirection: 'row', marginTop: '3%' }}>
-                                <View style={{ display: 'flex', flexDirection: 'row' }}>
-                                    <View style={{ position: 'relative' }}>
-                                        <Image
-                                            source={require('../../../../assets/imge/Home-imge/imge.jpg')}
-                                            style={{ width: 50, height: 50, borderRadius: 50 }}
-                                        />
-                                        <View style={{
-                                            width: 17,
-                                            height: 17,
-                                            backgroundColor: '#FFFFFF',
-                                            position: 'absolute',
-                                            bottom: 0,
-                                            right: 0,
-                                            borderRadius: 20
-                                        }}>
-                                            <View
-                                                style={{
-                                                    width: 15,
-                                                    height: 15,
-                                                    backgroundColor: 'green',
-                                                    position: 'absolute',
-                                                    bottom: 0,
-                                                    right: 0,
-                                                    borderRadius: 10
-                                                }}
+                                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                                    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                        <View style={{ position: 'relative' }}>
+                                            <Image
+                                                source={require('../../../../assets/imge/Home-imge/imge.jpg')}
+                                                style={{ width: 40, height: 40, borderRadius: 50 }}
                                             />
+                                            <View style={{
+                                                width: 13,
+                                                height: 13,
+                                                backgroundColor: '#FFFFFF',
+                                                position: 'absolute',
+                                                bottom: 0,
+                                                right: 0,
+                                                borderRadius: 200
+                                            }}>
+                                                <View
+                                                    style={{
+                                                        width: 12,
+                                                        height: 12,
+                                                        backgroundColor: 'green',
+                                                        position: 'absolute',
+                                                        bottom: 0,
+                                                        right: 0,
+                                                        borderRadius: 10
+                                                    }}
+                                                />
+                                            </View>
+                                        </View>
+                                        <View style={{ marginLeft: '5%' }}>
+                                            <Text style={{ color: 'rgba(94, 94, 98, 1)', fontSize: 10 }}>Təhvil verən</Text>
+                                            <Text style={{ color: 'rgba(70, 71, 74, 1)', fontSize: 11 }}>Əli Qasımov</Text>
                                         </View>
                                     </View>
-                                    <View style={{marginLeft:'5%'}}>
-                                        <Text style={{ color: 'rgba(94, 94, 98, 1)', fontSize:13 }}>Təhvil verən</Text>
-                                        <Text style={{ color: 'rgba(70, 71, 74, 1)', fontSize:15 }}>Əli Qasımov</Text>
+                                    <View style={{ marginRight: '5%' }}>
+                                        <TouchableOpacity>
+                                            <Callsvg width={30} height={30} />
+                                        </TouchableOpacity>
                                     </View>
                                 </View>
+
                             </View>
                         </View>
                         <View style={{ marginTop: '10%' }}>
-                            <Text style={{ color: "rgba(144, 144, 148, 1)", }}>Çatdırılma nöqtəsi</Text>
-                            <View style={{ display: 'flex', flexDirection: 'row' }}>
-                                <View>
-                                    <Text style={{ color: "rgba(16, 17, 20, 1)", marginTop: '2%', fontSize: 15 }}>Manti restaurant</Text>
-                                    <View style={{ display: 'flex', flexDirection: 'row', marginTop: '5%' }}>
-                                        <View style={{ display: 'flex', flexDirection: 'row' }}>
-                                            <KM width={25} height={25} />
-                                            <Text style={{ color: 'rgba(144, 144, 148, 1)' }}>62 km</Text>
+                            <Text style={{ color: "rgba(144, 144, 148, 1)", fontSize: 10 }}>Çatdırılma nöqtəsi</Text>
+                            <Text style={{ color: "rgba(16, 17, 20, 1)", fontSize: 11 }}>Manti restaurant</Text>
+                            <View style={{ display: 'flex', flexDirection: 'row', marginTop: '2%' }}>
+                                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                                    <View style={{ display: 'flex', flexDirection: 'row' }}>
+                                        <View style={{ position: 'relative' }}>
+                                            <Image
+                                                source={require('../../../../assets/imge/Home-imge/imge.jpg')}
+                                                style={{ width: 40, height: 40, borderRadius: 50 }}
+                                            />
+                                            <View style={{
+                                                width: 13,
+                                                height: 13,
+                                                backgroundColor: '#FFFFFF',
+                                                position: 'absolute',
+                                                bottom: 10,
+                                                right: 0,
+                                                borderRadius: 20
+                                            }}>
+                                                <View
+                                                    style={{
+                                                        width: 12,
+                                                        height: 12,
+                                                        backgroundColor: 'green',
+                                                        position: 'absolute',
+                                                        bottom: 0,
+                                                        right: 0,
+                                                        borderRadius: 10
+                                                    }}
+                                                />
+                                            </View>
+                                        </View>
+                                        <View style={{ marginLeft: '5%' }}>
+                                            <Text style={{ color: 'rgba(94, 94, 98, 1)', fontSize: 10 }}>Təhvil alan</Text>
+                                            <Text style={{ color: 'rgba(70, 71, 74, 1)', fontSize: 11 }}>Nadir Osmanov</Text>
                                         </View>
                                     </View>
-                                </View>
-                                <View style={{ marginLeft: '10%', marginTop: '2%' }}>
-                                    <Text style={{ color: 'rgba(16, 17, 20, 1)', fontSize: 23, textAlign: 'right' }}>45 kg</Text>
+                                    <View style={{ alignSelf: "center", marginRight: '5%' }}>
+                                        <TouchableOpacity style={{ alignSelf: 'center' }}>
+                                            <Callsvg width={30} height={30} />
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
                             </View>
                         </View>
                     </View>
+                </View>
+                <View style={{marginTop:'5%'}}>
+                    <TouchableOpacity style={{display:'flex', flexDirection:'row'}}>
+                        <Text style={{color:'rgba(64, 62, 62, 1)', fontSize:10}}>
+                            Daha çox bax
+                        </Text>
+                        <Moresvg width={20} height={20}/>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
