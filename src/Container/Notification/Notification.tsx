348f6188950ff4ece36c92af76a84b7512e05c27
Nizam <ramazanov570633@gmail.com>
2024-04-06 13:02:18 +0400

    Notification update

--- src/Container/Notification/Notification.tsx
+++ src/Container/Notification/Notification.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Null_Page from '../../Component/NotificationComponent/Null_Page/Null_Page'
+import { SCREEN_WIDTH } from '../../Utils/common'
 
 const Notification = () => {
   return (
-    <View>
-      <Text>Notification</Text>
+    <View style={{backgroundColor:'#FFFFFF', flex:1}}>
+      <Text style={{ color: 'rgba(16, 17, 20, 1)', width: SCREEN_WIDTH - 40, alignSelf: 'center', marginTop: '3%', fontSize: 17 }}>Bildirişlər</Text>
+      <Null_Page />
     </View>
   )
 }
