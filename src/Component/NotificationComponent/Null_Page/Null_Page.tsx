f281244a0ec6954f38cf3526a27619b31ffcaba3
Nizam <ramazanov570633@gmail.com>
2024-04-07 19:18:18 +0400

    Notification update 3

--- src/Component/NotificationComponent/Null_Page/Null_Page.tsx
+++ src/Component/NotificationComponent/Null_Page/Null_Page.tsx
@@ -1,22 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Date from '../../../assets/imge/Time-imge/Frame.svg'
-import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../../../Utils/common'
-const Null_Page = () => {
-    return (
-        <View style={{ flex: 1 , justifyContent:'center'}}>
-            <View style={{}}>
-                <View style={{ alignSelf: 'center' }}>
-                    <Date  width={250} height={250}/>
-                </View>
-                <View style={{marginTop: '3%' }}>
-                    <Text style={{ color: 'rgba(70, 71, 74, 1)', textAlign: 'center', fontSize: 22, width: SCREEN_WIDTH /1.5, alignSelf:'center' }}>Bildirişləriniz yoxdur</Text>
-                    <Text style={{ color: 'rgba(118, 119, 122, 1)', textAlign: 'center', fontSize: 15 , width: SCREEN_WIDTH /1.1 , alignSelf:'center', marginTop:'2%'}}>Yeni bildirişləriniz burada göstəriləcək</Text>
-                </View>
-            </View>
-        </View>
-    )
-}
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import Date from '../../../assets/imge/Time-imge/Frame.svg'
+// import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../../../Utils/common'
+// const Null_Page = () => {
+//     return (
+//         <View style={{ flex: 1 , justifyContent:'center'}}>
+//             <View style={{}}>
+//                 <View style={{ alignSelf: 'center' }}>
+//                     <Date  width={250} height={250}/>
+//                 </View>
+//                 <View style={{marginTop: '3%' }}>
+//                     <Text style={{ color: 'rgba(70, 71, 74, 1)', textAlign: 'center', fontSize: 22, width: SCREEN_WIDTH /1.5, alignSelf:'center' }}>Bildirişləriniz yoxdur</Text>
+//                     <Text style={{ color: 'rgba(118, 119, 122, 1)', textAlign: 'center', fontSize: 15 , width: SCREEN_WIDTH /1.1 , alignSelf:'center', marginTop:'2%'}}>Yeni bildirişləriniz burada göstəriləcək</Text>
+//                 </View>
+//             </View>
+//         </View>
+//     )
+// }
 
 
-export default Null_Page
+// export default Null_Page
