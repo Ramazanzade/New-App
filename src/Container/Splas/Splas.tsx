8fb3984d1cf093707eab490c0b637295a2585840
Nizam <ramazanov570633@gmail.com>
2024-04-24 10:17:13 +0400

    Last update end final

--- src/Container/Splas/Splas.tsx
+++ src/Container/Splas/Splas.tsx
@@ -1,96 +1,48 @@
-import { View, Text, Animated, Easing, Dimensions } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
-import LogoName from '../../assets/imge/Splas-imge/cuxie.svg'
-import Imegecenter from '../../assets/imge/Splas-imge/Mail sent-bro 1.svg'
-import LogoName2 from '../../assets/imge/Splas-imge/bluename.svg'
-import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../../Utils/common'
+import { View, Animated, Easing, Dimensions } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import LogoName from '../../assets/imge/Splas-imge/cuxie.svg';
+import Imegecenter from '../../assets/imge/Splas-imge/Mail sent-bro 1.svg';
+import LogoName2 from '../../assets/imge/Splas-imge/bluename.svg';
+
 const window = Dimensions.get('window');
 
 const Splas = ({ navigation }: any) => {
-  const [bacround, setbacround] = useState('#FFFFFF')
-  const opacity = useRef(new Animated.Value(0)).current;
-  const sizeAnim = useRef(new Animated.Value(0)).current;
+  const [background, setBackground] = useState<any>(new Animated.Value(0));
 
   useEffect(() => {
-    Animated.timing(sizeAnim, {
-      toValue: 5,
-      duration: 5000,
-      useNativeDriver: false,
-    }).start();
-  }, [sizeAnim]);
-
-  const sizeStyle = {
-    width: sizeAnim.interpolate({
-      inputRange: [0, 1, 2],
-      outputRange: [0, window.width / 2, window.width],
-    }),
-    height: sizeAnim.interpolate({
-      inputRange: [0, 1, 2],
-      outputRange: [0, window.width / 2, window.width],
-    }),
-    borderRadius: sizeAnim.interpolate({
-      inputRange: [0, 1, 2],
-      outputRange: [window.width / 2, window.width / 2, window.width / 2]
-    }),
-    backgroundColor: 'blue',
-  };
-  useEffect(() => {
     setTimeout(() => {
-      // navigation.navigate('OnboardingScreen')
-      setbacround('rgba(0, 80, 155, 1)')
-    }, 7000)
-
-    setTimeout(() => {
-      Animated.timing(opacity, {
+      Animated.timing(background, {
         toValue: 1,
-        duration: 2000,
-        useNativeDriver: true,
-        easing: Easing.linear
+        duration: 1000,
+        useNativeDriver: false,
       }).start();
-    }, 2000);
-  }, [])
+    }, 3000);
+    setTimeout(() => {
+      navigation.navigate('OnboardingScreen')
+    }, 5000);
+  }, []);
+
+  const backgroundColor = background.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#FFFFFF', 'rgba(0, 80, 155, 1)'],
+  });
 
   return (
-    <Animated.View style={
-      {
-        backgroundColor: '#FFFFFF',
-        flex: 1
-      }
-    }>
-      <View style={{ alignSelf: 'center', flex: 1, }}>
-
+    <Animated.View style={{ backgroundColor, flex: 1 }}>
+      <View style={{ alignSelf: 'center', flex: 1 }}>
         <View style={{ alignSelf: 'center' }}>
-          {bacround === '#FFFFFF'
-            ?
-            <Animated.View style={{
-              alignSelf: 'center', opacity, transform: [
-                {
-                  translateX: opacity.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, 0],
-                  }),
-                },
-              ],
-            }}>
-              <LogoName2 height={300} width={300} />
-            </Animated.View >
-            :
+          {background === '#FFFFFF' ? (
+            <LogoName2 height={300} width={300} />
+          ) : (
             <LogoName height={300} width={300} />
-          }
-        </View>
-        <View style={{ position: 'absolute', flex: 1, zIndex: -1, alignSelf:'center'}}>
-          <Animated.View style={[sizeStyle]} />
+          )}
         </View>
         <View style={{ alignSelf: 'center' }}>
           <Imegecenter width={500} height={400} />
         </View>
       </View>
-
     </Animated.View>
-  )
-}
+  );
+};
 
-export default Splas
-
-
-
+export default Splas;
